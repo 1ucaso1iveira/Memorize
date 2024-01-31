5628bc57f9124ffe1421ae2cb77ae715b9daa84c
1ucaso1iveira <lucasoliveiraahh@gmail.com>
2024-01-30 22:26:49 -0300

    First Commit.

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Lucas Oliveira on 01/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🍍","🍎","🥑","🍇", "🍋","🍉","🥥","🥭","🍒","🥝","🫐","🍑"]
+   @State var emojiCount: Int = 4
+    
+    var body: some View {
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id:  \.self){ emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+        .foregroundColor(Color.blue)
+        
+    }
+    
+    var remove: some View{
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add: some View{
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View{
+    var content: String
+   @State var isFaceUp: Bool = true
+    
+    var body: some View{
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lucas Oliveira on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
